52da7741a3eae9ec05251f575bd6e17958347bb8
neverw1nter77 <neverw1nter100@outlook.com>
2026-06-17 10:08:51 +0700

    creating a basic API client and pet_schema

---
+++ tools/fakers.py
@@ -0,0 +1,29 @@
+from faker import Faker
+
+class Fake:
+
+    def __init__(self, faker: Faker):
+        """
+        :param faker: Экземпляр класса Faker, который будет использоваться для генерации данных.
+        """
+        self.faker = faker
+
+    def pet_name(self) -> str:
+        return self.faker.first_name()
+
+    def status(self) -> str:
+        return self.faker.random_element(["available", "pending", "sold"])
+
+    def photo_url(self) -> str:
+        return self.faker.image_url()
+
+    def category_name(self) -> str:
+        return self.faker.random_element(["dog", "cat", "bird"])
+
+    def tag_name(self) -> str:
+        return self.faker.word()
+
+    def id(self) -> int:
+        return self.faker.random_int(min=1, max=10000)
+
+fake = Fake(faker=Faker())
