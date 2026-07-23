5c77e1d92abad3110f21d2d7c5166fab7ac72c26
neverw1nter77 <neverw1nter100@outlook.com>
2026-07-23 09:48:18 +0700

    Added run autotests on CI

--- clients/pet/pet_client.py
+++ clients/pet/pet_client.py
@@ -11,7 +11,7 @@ class PetClient(APIClient):
     @allure.step("Create pet")
     def create_pet_api(self, request: CreatePetRequestSchema) -> Response:
         """
-        Создание пит омца.
+        Создание питомца.
 
         :param request: Тело запроса с данными питомца
         :return: Ответ от сервера (httpx.Response)
